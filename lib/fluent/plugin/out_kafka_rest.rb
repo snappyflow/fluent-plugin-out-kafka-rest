83f1066a974df2bfe4b9059cd52ea8a3226d7471
SabariArunkumarP <51473820+SabariArunkumarP@users.noreply.github.com>
2021-02-12 17:59:10 +0530

    Support Auth token and msg format fix

--- lib/fluent/plugin/out_kafka_rest.rb
+++ lib/fluent/plugin/out_kafka_rest.rb
@@ -1,170 +1,170 @@
 class Fluent::KafkaRestOutput < Fluent::Output
-  Fluent::Plugin.register_output('kafka_rest', self)
-
-  def initialize
-    super
-    require 'net/https'
-    require 'openssl'
-    require 'uri'
-    require 'yajl'
-    require 'base64'
-  end
-
-  # https or http
-  config_param :use_ssl, :bool, :default => false
-
-  # include tag
-  config_param :include_tag, :bool, :default => false
-
-  # include timestamp
-  config_param :include_timestamp, :bool, :default => false
-
-  # Endpoint URL ex. localhost.local/api/
-  config_param :endpoint_url, :string
-
-  # HTTP method
-  config_param :http_method, :string, :default => :post
+    Fluent::Plugin.register_output('kafka_rest', self)
   
-  # json_bin ( Should support avro in the future)
-  config_param :serializer, :string, :default => :json_bin
-
-  # Content-Type
-  config_param :content_type, :string, :default => 'application/json'
-
-  # Simple rate limiting: ignore any records within `rate_limit_msec`
-  # since the last one.
-  config_param :rate_limit_msec, :integer, :default => 0
-
-  # nil | 'none' | 'basic'
-  config_param :authentication, :string, :default => nil 
-  config_param :username, :string, :default => ''
-  config_param :password, :string, :default => ''
-
-  def configure(conf)
-    super
-
-    @use_ssl = conf['use_ssl']
-    @include_tag = conf['include_tag']
-    @include_timestamp = conf['include_timestamp']
-
-    serializers = [:json_bin]  # Should support :avro in the future
-    @serializer = if serializers.include? @serializer.intern
-                    @serializer.intern
-                  else
-                    :json_bin
-                  end
-
-    @content_type = conf['content_type']
-
-    # Kafka REST Proxy accepts only POST method at the moment
-    http_methods = [:post]
-    @http_method = if http_methods.include? @http_method.intern
-                    @http_method.intern
-                  else
-                    :post
-                  end
-
-    @auth = case @authentication
-            when 'basic' then :basic
-            else
-              :none
-            end
-  end
-
-  def start
-    super
-  end
-
-  def shutdown
-    super
-  end
-
-  def format_url(tag, time, record)
-    @endpoint_url
-  end
-
-  def set_body(req, tag, time, record)
-    # TODO: Add avro support
-    if @include_tag
-      record['tag'] = tag
+    def initialize
+      super
+      require 'net/https'
+      require 'openssl'
+      require 'uri'
+      require 'yajl'
+      require 'base64'
     end
-    if @include_timestamp
-      record['timestamp'] = Time.now.to_i
-    end 
-    if @serializer == :json_bin
-      set_json_body(req, record)
-    # elsif @serializer == :avro
-    #   set_avro_body(req, record)
-    end
-    req
-  end
-
-  def set_header(req, tag, time, record)
-    req
-  end
-
-  def set_json_body(req, data)
-    dumped_data = Yajl.dump(data)
-    encoded_data = Base64.encode64(dumped_data)
-    req.body = Yajl.dump({ "records" => [{ "value" => encoded_data }] })
-    req['Content-Type'] = 'application/vnd.kafka.binary.v1+json'
-  end
-
-  def set_avro_body(req, data)
-    # TODO: Implement avro body parser
-  end
-
-  def create_request(tag, time, record)
-    url = format_url(tag, time, record)
-    uri = URI.parse(url)
-    req = Net::HTTP.const_get(@http_method.to_s.capitalize).new(uri.path)
-    set_body(req, tag, time, record)
-    set_header(req, tag, time, record)
-    return req, uri
-  end
-
-  def send_request(req, uri)    
-    is_rate_limited = (@rate_limit_msec != 0 and not @last_request_time.nil?)
-    if is_rate_limited and ((Time.now.to_f - @last_request_time) * 1000.0 < @rate_limit_msec)
-      $log.info('Dropped request due to rate limiting')
-      return
-    end
+  
+    # https or http
+    config_param :use_ssl, :bool, :default => false
+  
+    # include tag
+    config_param :include_tag, :bool, :default => false
+  
+    # include timestamp
+    config_param :include_timestamp, :bool, :default => false
+  
+    # Endpoint URL ex. localhost.local/api/
+    config_param :endpoint_url, :string
+  
+    # HTTP method
+    config_param :http_method, :string, :default => :post
     
-    res = nil
-    begin
-      if @auth and @auth == :basic
-        req.basic_auth(@username, @password)
-      end
-      @last_request_time = Time.now.to_f
-      https = Net::HTTP.new(uri.host, uri.port)
-      https.use_ssl = @use_ssl
-      https.ca_file = OpenSSL::X509::DEFAULT_CERT_FILE 
-#      https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      res = https.start {|http| http.request(req) }
-    rescue IOError, EOFError, SystemCallError
-      # server didn't respond
-      $log.warn "Net::HTTP.#{req.method.capitalize} raises exception: #{$!.class}, '#{$!.message}'"
-    end
-    unless res and res.is_a?(Net::HTTPSuccess)
-      res_summary = if res
-                      "#{res.code} #{res.message} #{res.body}"
+    # json_bin ( Should support avro in the future)
+    config_param :serializer, :string, :default => :json_bin
+  
+    # Content-Type
+    config_param :content_type, :string, :default => 'application/json'
+  
+    # Simple rate limiting: ignore any records within `rate_limit_msec`
+    # since the last one.
+    config_param :rate_limit_msec, :integer, :default => 0
+  
+    # nil | 'none' | 'basic'
+    #config_param :authentication, :string, :default => nil 
+    config_param :username, :string, :default => ''
+    config_param :password, :string, :default => ''
+    config_param :token, :string, :default => ''
+  
+    def configure(conf)
+      super
+  
+      @use_ssl = conf['use_ssl']
+      @include_tag = conf['include_tag']
+      @include_timestamp = conf['include_timestamp']
+  
+      serializers = [:json_bin]  # Should support :avro in the future
+      @serializer = if serializers.include? @serializer.intern
+                      @serializer.intern
                     else
-                      "res=nil"
+                      :json_bin
                     end
-      $log.warn "failed to #{req.method} #{uri} (#{res_summary})"
+  
+      @content_type = conf['content_type']
+  
+      # Kafka REST Proxy accepts only POST method at the moment
+      http_methods = [:post]
+      @http_method = if http_methods.include? @http_method.intern
+                      @http_method.intern
+                    else
+                      :post
+                    end
+  
+      # @auth = case @authentication
+      #         when 'basic' then :basic
+      #         when 'bearer' then :bearer
+      #         else
+      #           :none
+      #         end
+    end
+  
+    def start
+      super
+    end
+  
+    def shutdown
+      super
+    end
+  
+    def format_url(tag, time, record)
+      @endpoint_url
+    end
+  
+    def set_body(req, tag, time, record)
+      # TODO: Add avro support
+      if @include_tag
+        record['tag'] = tag
+      end
+      if @include_timestamp
+        record['timestamp'] = Time.now.to_i
+      end 
+      if @serializer == :json_bin
+        set_json_body(req, record)
+      # elsif @serializer == :avro
+      #   set_avro_body(req, record)
+      end
+      req
+    end
+  
+    def set_header(req, tag, time, record)
+      req['Content-Type'] = 'application/vnd.kafka.json.v2+json'
+      req['Accept'] = 'application/vnd.kafka.v2+json'
+      req['Authorization'] = @token
+      req
+    end
+  
+    def set_json_body(req, data)
+     # req.body = Yajl.dump({ "records" => [{ "value" => dumped_data }] })
+      req.body = Yajl.dump({ "records" => [{ "value" => data }] })
+    end
+  
+    def set_avro_body(req, data)
+      # TODO: Implement avro body parser
+    end
+  
+    def create_request(tag, time, record)
+      url = format_url(tag, time, record)
+      uri = URI.parse(url)
+      req = Net::HTTP.const_get(@http_method.to_s.capitalize).new(uri.path)
+      set_body(req, tag, time, record)
+      set_header(req, tag, time, record)
+      return req, uri
+    end
+  
+    def send_request(req, uri)    
+      is_rate_limited = (@rate_limit_msec != 0 and not @last_request_time.nil?)
+      if is_rate_limited and ((Time.now.to_f - @last_request_time) * 1000.0 < @rate_limit_msec)
+        $log.info('Dropped request due to rate limiting')
+        return
+      end
+      
+      res = nil
+      begin
+        @last_request_time = Time.now.to_f
+        https = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl = @use_ssl
+        https.ca_file = OpenSSL::X509::DEFAULT_CERT_FILE 
+  #      https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        res = https.start {|http| http.request(req) }
+      rescue IOError, EOFError, SystemCallError
+        # server didn't respond
+        $log.warn "Net::HTTP.#{req.method.capitalize} raises exception: #{$!.class}, '#{$!.message}'"
+      end
+      unless res and res.is_a?(Net::HTTPSuccess)
+        res_summary = if res
+                        "#{res.code} #{res.message} #{res.body}"
+                      else
+                        "res=nil"
+                      end
+        $log.warn "failed to #{req.method} #{uri} (#{res_summary})"
+      end
+    end
+  
+    def handle_record(tag, time, record)
+      req, uri = create_request(tag, time, record)
+      send_request(req, uri)
+    end
+  
+    def emit(tag, es, chain)
+      es.each do |time, record|
+        handle_record(tag, time, record)
+      end
+      chain.next
     end
   end
-
-  def handle_record(tag, time, record)
-    req, uri = create_request(tag, time, record)
-    send_request(req, uri)
-  end
-
-  def emit(tag, es, chain)
-    es.each do |time, record|
-      handle_record(tag, time, record)
-    end
-    chain.next
-  end
-end
